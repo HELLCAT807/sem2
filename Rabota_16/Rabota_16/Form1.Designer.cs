7eb425ca18e9d157a53ff5a46d7900c74cd30010
HELLCAT807 <Andreybmx1998@gmail.com>
2025-11-18 08:15:49 +0300

    Add files via upload

---
+++ Rabota_16/Rabota_16/Form1.Designer.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace Rabota_16
+{
+    partial class Knopka
+    {
+        private IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Knopka
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 400);
+            this.Name = "Knopka";
+            this.Text = "Лабораторная работа 16. Вариант 12";
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.BackColor = System.Drawing.Color.White;
+            this.Load += new System.EventHandler(this.Knopka_Load);
+            this.ResumeLayout(false);
+        }
+    }
+}
+
---
+++ Rabota_16/Rabota_16/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Rabota_16
+{
+    public partial class Knopka : Form
+    {
+        private bool captured = false;
+        private Button RunningButton;
+
+        public Knopka()
+        {
+            InitializeComponent();
+            InitializeButton();
+        }
+
+        private void InitializeButton()
+        {
+            RunningButton = new Button();
+            RunningButton.Text = "Нажми меня!";
+            RunningButton.Size = new Size(100, 50);
+            RunningButton.BackColor = Color.Red;
+            Controls.Add(RunningButton);
+
+            RunningButton.MouseHover += RunningButton_MouseHover;
+            DoubleClick += endOfProgramm;
+            Resize += Knopka_Resize;
+        }
+
+        private void Knopka_Load(object sender, EventArgs e)
+        {
+            RunningButton.Location = new Point(
+                (ClientSize.Width - RunningButton.Width) / 2,
+                (ClientSize.Height - RunningButton.Height) / 2
+            );
+        }
+
+        private void RunningButton_MouseHover(object sender, EventArgs e)
+        {
+            if (!captured)
+            {
+                RandomPosition();
+            }
+        }
+
+        private void RandomPosition()
+        {
+            Random random = new Random();
+
+            int maxX = ClientSize.Width - RunningButton.Width;
+            int maxY = ClientSize.Height - RunningButton.Height;
+
+            if (maxX < 0) maxX = 0;
+            if (maxY < 0) maxY = 0;
+
+            RunningButton.Location = new Point(
+                random.Next(0, maxX + 1),
+                random.Next(0, maxY + 1)
+            );
+        }
+
+        private void endOfProgramm(object sender, EventArgs e)
+        {
+            captured = true;
+            RunningButton.Text = "Поймал";
+            RunningButton.BackColor = Color.Green;
+            RunningButton.MouseHover -= RunningButton_MouseHover;
+        }
+
+        private void Knopka_Resize(object sender, EventArgs e)
+        {
+            if (RunningButton == null) return;
+
+            int x = RunningButton.Location.X;
+            int y = RunningButton.Location.Y;
+
+            int maxX = ClientSize.Width - RunningButton.Width;
+            int maxY = ClientSize.Height - RunningButton.Height;
+
+            if (maxX < 0) maxX = 0;
+            if (maxY < 0) maxY = 0;
+
+            if (x > maxX) x = maxX;
+            if (y > maxY) y = maxY;
+            if (x < 0) x = 0;
+            if (y < 0) y = 0;
+
+            RunningButton.Location = new Point(x, y);
+        }
+    }
+}
